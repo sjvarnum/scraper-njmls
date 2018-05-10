5105ccf18d827f28d16e8eeb067b97cc13e3b6ad
Shane Varnum <shane.varnum@gmail.com>
2018-05-10 12:03:46 -0400

    Coded method for retrieving municipalties

---
+++ njmls_agent_scraper_v3.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+# import pandas as pd
+# import re
+
+
+class Scraper(object):
+
+    agent = (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
+    )
+
+    def get_municipalties(self):
+
+        muni_url = ("http://www.njmls.com/listings/index.cfm?action="
+                    "xhr.multiple_town_select_new#tab1")
+
+        self.r = requests.get(muni_url, headers={'User-Agent': Scraper.agent})
+        self.c = self.r.content
+        self.soup = BeautifulSoup(self.c, 'html.parser')
+
+        municipalities = []
+        for i in self.soup.find_all('input', {'class': 'multitown_checks'}):
+            value = i.get('value')
+            municipalities.append(value)
+        print('this would be a list of munis')
+        return municipalities
+
+    def get_counties():
+        pass
+
+
+if __name__ == '__main__':
+    muni = Scraper()
+    muni.get_municipalties()
