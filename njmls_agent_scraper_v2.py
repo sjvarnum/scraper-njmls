64de3abb7d935724704b3f1beb2140633fd78b1a
Shane Varnum <shane.varnum@gmail.com>
2018-05-09 21:10:58 -0400

    Changed code to get cities from the local web site instead of wikipedia

--- njmls_agent_scraper_v2.py
+++ njmls_agent_scraper_v2.py
@@ -1,4 +1,4 @@
-""" This version gets the list of municipalities from Wikipedia """
+""" This version gets the list of municipalities from NJMLS web site """
 
 
 from bs4 import BeautifulSoup
@@ -12,44 +12,49 @@ agent = (
     '(KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299'
 )
 
-wiki_url = 'https://en.wikipedia.org/wiki/List_of_municipalities_in_New_Jersey'
-r = requests.get(wiki_url, headers={'User-Agent': agent})
+muni_url = ("""http://www.njmls.com/listings/index.cfm?action=
+            xhr.multiple_town_select_new#tab1""")
+
+r = requests.get(muni_url, headers={'User-Agent': agent})
 c = r.content
 soup = BeautifulSoup(c, 'html.parser')
 
+
 municipalities = []
-table = soup.find('table', attrs={'class': 'wikitable sortable'})
-rows = table.find_all('tr')
-for row in rows[1:]:
-    cols = row.find_all('td')
-    cols = [element.text.strip() for element in cols]
+for i in soup.find_all('input', {'class': 'multitown_checks'}):
+    value = i.get('value')
+    municipalities.append(value)
+
+county_list = []
+for i in municipalities:
     my_dict = {}
-    my_dict['City'] = cols[1]
-    my_dict['County'] = cols[2]
-    municipalities.append(my_dict)
+    my_dict['City'] = i.split(', ')[0]
+    my_dict['County'] = i.split(', ')[2]
+    county_list.append(my_dict)
 
 
-base_url = 'http://www.njmls.com/members/index.cfm?action=dsp.results&city='
-
-url_list = []
-for item in municipalities:
-    city = item['City']
-    county = item['County']
-    url = f'{base_url}{city}'.replace(' ', '+')
-    url = f'{url}&county={county}'
-    url_list.append(url)
-url_list
+base_url = 'http://www.njmls.com/members/index.cfm?action=dsp.results'
 
 agent_list = []
-for url in url_list:
-
-    r = requests.get(url, headers={'User-Agent': agent})
+for item in county_list:
+    city = item['City']
+    county = item['County']
+    params = {'city': city, 'county': county}
+    r = requests.get(base_url, params=params, headers={'User-Agent': agent})
     c = r.content
     soup = BeautifulSoup(c, 'html.parser')
     agents = soup.find_all('div', {'class': 'realtor-info'})
 
     for i in agents[:-1]:
         my_dict = {}
+        try:
+            my_dict['City'] = city
+        except (AttributeError, IndexError):
+            None
+        try:
+            my_dict['County'] = county
+        except (AttributeError, IndexError):
+            None
         try:
             my_dict['Name'] = i.find('strong').text
         except (AttributeError, IndexError):
@@ -93,6 +98,8 @@ for url in url_list:
         agent_list.append(my_dict)
 
 df = pd.DataFrame(agent_list)
-agent_df = df[['Name', 'Title', 'Agency', 'Office Number',
-               'Contact Number', 'Email']].drop_duplicates()
+agent_df = (df[['Name', 'Title', 'Agency', 'Office Number',
+                'Contact Number', 'Email']]
+            .drop_duplicates()
+            .sort_values('Name'))
 agent_df.to_csv('njmls_agents_20180509.csv', index=False)
